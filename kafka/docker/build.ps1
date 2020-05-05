592b252dbd18010c82fbac1fc0c683cbcc3da6f1
parris3142 <parris3142@hotmail.com>
2020-05-05 22:40:59 +0100

    Setting up dockerfiles for zookeeper and Kafka

--- kafka/docker/build.ps1
+++ kafka/docker/build.ps1
@@ -1,1 +1,2 @@
- docker build -f .\Dockerfile-kafka-all-in-one . -t kafka-ubuntu/1.0
+ docker build -f .\Dockerfile-Ubuntu-Java . -t ubuntu-java:1.0
+ docker build -f .\Dockerfile-ZooKeeper . -t zookeeper:1.0
