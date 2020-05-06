4abc5b4620eb94e325a08871856fb9d74970212e
parris3142 <parris3142@hotmail.com>
2020-05-06 23:18:49 +0100

    refactor ready to create Kafka docker image

--- kafka/docker/build.ps1
+++ kafka/docker/build.ps1
@@ -1,2 +1,3 @@
  docker build -f .\Dockerfile-Ubuntu-Java . -t ubuntu-java:1.0
  docker build -f .\Dockerfile-ZooKeeper . -t zookeeper:1.0
+ docker build -f .\Dockerfile-ZooKeeper-server . -t zookeeper-server:1.0
