7a3d2bf04f51f460d2414981cd9453ce9373a6d4
parris3142 <parris3142@hotmail.com>
2020-05-08 21:22:30 +0100

    added option to set Zookeeper as env var in Pod spec

--- kafka/docker/kafka/kafka-run.sh
+++ kafka/docker/kafka/kafka-run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd /opt/kafka || exit
+
+if [[ -z "${ZOOKEEPER_SERVER}" ]]; then
+  echo -------------------Use default server.properties -----------------
+else
+  echo -------------------Override ZooKeeper server----------------------
+  sed "s/zookeeper-server:2181/${ZOOKEEPER_SERVER}/" ./config/server.properties > ./config/new_sp
+  mv ./config/new_sp ./config/server.properties
+fi
+
 ./bin/kafka-server-start.sh ./config/server.properties
 
 while true; do
