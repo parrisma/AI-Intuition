ea8e77251dce93616a4f13225866901dd7241e06
parris3142 <parris3142@hotmail.com>
2020-05-09 14:33:03 +0100

    re work of dir structure, got rid of top level Kafka dir so docker is now in root

--- kafka/docker/build.ps1
+++
@@ -1,5 +0,0 @@
- docker build -f .\Dockerfile-Ubuntu-Java . -t ubuntu-java:1.0
- docker build -f .\Dockerfile-ZooKeeper . -t zookeeper:1.0
- docker build -f .\Dockerfile-ZooKeeper-server . -t zookeeper-server:1.0
- docker build -f .\Dockerfile-Kafka . -t kafka:1.0
- docker build -f .\Dockerfile-Kafka-server . -t kafka-server:1.0
