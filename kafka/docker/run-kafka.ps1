5491318cd46d48d3f636326d6f690309b63ce926
parris3142 <parris3142@hotmail.com>
2020-05-09 12:36:47 +0100

    fix server properties to add listeners

--- kafka/docker/run-kafka.ps1
+++ kafka/docker/run-kafka.ps1
@@ -1,4 +1,4 @@
 docker network create ai-net
 docker run -d --rm -h zookeeper-server --name zookeeper-server --network ai-net zookeeper-server:1.0
-docker run -d --rm -h kafka-server --name kafka-server -p 9092:9092 --network ai-net kafka-server:1.0
+docker run -d --rm -h kafka-server --name kafka-server -p 9092:9092 --network ai-net -e ZOOKEEPER_SERVER=zookeeper-server:2181 -e KAFKA_PLAINTEXT=localhost:9092 -e KAFKA_EXTERNAL=localhost:30105 kafka-server:1.0
 
