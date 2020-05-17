a4d6d8d1a6449b4419a802c1095fbc933088bb4f
parris3142 <parris3142@hotmail.com>
2020-05-17 10:32:39 +0100

    update to pyenv checker

--- env/test.py
+++ env/test.py
@@ -1,1 +1,22 @@
-exit(-1)
+"""
+Simple test to validate the install of the python libraries.
+"""
+try:
+    import tensorflow as tf
+    from tensorflow import keras
+
+    print("Tensor flow & Keras import OK")
+except Exception as e:
+    print("Failed to import tensorflow & Keras libraries {}".format(str(e)))
+    exit(-1)
+
+try:
+    from kafka import KafkaConsumer
+    from kafka import KafkaProducer
+
+    print("Kafka import OK")
+except Exception as e:
+    print("Failed to import Kafka libraries {}".format(str(e)))
+    exit(-1)
+
+exit(0)
