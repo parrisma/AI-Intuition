abf609272be27c334a233a4029ec93a75a91f124
parris3142 <parris3142@hotmail.com>
2020-05-19 22:06:13 +0100

    changed kafka container embedded run script to overide external & internal listeners based on env variables

--- docker/kafka/kafka-run.sh
+++ docker/kafka/kafka-run.sh
@@ -6,18 +6,30 @@ if [[ -z "${ZOOKEEPER_SERVER}" ]]; then
   echo -------------------Use default Zookeeper in server.properties -----------------
 else
   echo -------------------Override ZooKeeper server----------------------
-  sed "s/zookeeper-server:2181/${ZOOKEEPER_SERVER}/g" ./config/server.properties > ./config/new_sp
-  mv ./config/new_sp ./config/server.properties
+sed "s/zookeeper-server:2181/${ZOOKEEPER_SERVER}/g" ./config/server.properties > ./config/new_sp
+mv ./config/new_sp ./config/server.properties
 fi
 
-./bin/kafka-server-start.sh ./config/server.properties
+if [[ -z "${EXT_KAFKA_HOST}" ]] || [[ -z "${EXT_KAFKA_PORT}" ]]; then
+  echo "ERROR: Script required environment variables EXT_KAFKA_HOST and EXT_KAFKA_PORT to be set"
+  exit 1
+fi
 
-# Just for testing, so we can stop the current Kafka and fix settings etc
-# without the container exiting.
+echo " Override the EXTERNAL Kafka listener and advertised listener : ${EXT_KAFKA_HOST}:${EXT_KAFKA_PORT}"
+sed "s/EXT-KAFKA-HOST/${EXT_KAFKA_HOST}/g" ./config/server.properties > ./config/new_sp
+mv ./config/new_sp ./config/server.properties
+sed "s/EXT-KAFKA-PORT/${EXT_KAFKA_PORT}/g" ./config/server.properties > ./config/new_sp
+mv ./config/new_sp ./config/server.properties
 
-# Un comment for testing, where Kafka fails to start and you want to see logs & restart with new settings
-# docker exec -it <kafka container id> /bin/bash
-#while true; do
-#  sleep 10
-#  echo Tick
-#done
+if [[ -z "${DOCKER_KAFKA_HOST}" ]] || [[ -z "${DOCKER_KAFKA_PORT}" ]]; then
+  echo "ERROR: Script required environment variables DOCKER_KAFKA_HOST and DOCKER_KAFKA_PORT to be set"
+  exit 1
+fi
+
+echo " Override the INTERNAL Kafka listener and advertised listener : ${DOCKER_KAFKA_HOST}:${DOCKER_KAFKA_PORT}"
+sed "s/DOCKER-KAFKA-HOST/${DOCKER_KAFKA_HOST}/g" ./config/server.properties > ./config/new_sp
+mv ./config/new_sp ./config/server.properties
+sed "s/DOCKER-KAFKA-PORT/${DOCKER_KAFKA_PORT}/g" ./config/server.properties > ./config/new_sp
+mv ./config/new_sp ./config/server.properties
+
+exit 0
