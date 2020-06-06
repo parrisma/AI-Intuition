8ef114ed0cf7ffabffa893e190d522775daee92a
parris3142 <parris3142@hotmail.com>
2020-06-06 16:39:46 +0100

    State added to message map yml

---
+++ journey11/src/main/kpubsubai/compile.ps1
@@ -0,0 +1,11 @@
+# conda activate notebook_gpu_netx
+# make sure conda\bin conda\scripts are on the system file path
+#
+. ..\..\..\..\ps1\ProtoComplie.ps1
+
+$local = "d:/Dev/devroot/AI-Intuition/journey11/src/main/protobuf"
+$rmt = "/var/data"
+
+ProtoCompile -LocalDir $local -ConatinerDir $rmt -Filename pb_uniqueworkref.proto
+
+
