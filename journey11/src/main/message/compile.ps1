dace30286c8e2337250969547dfa0b663bd2cbad
parris3142 <parris3142@hotmail.com>
2020-06-12 06:25:46 +0100

    added kpubsubai settings

--- journey11/src/main/message/compile.ps1
+++
@@ -1,9 +0,0 @@
-# Compile the ProtoBuf files
-#
-. ..\..\..\..\ps1\ProtoComplie.ps1
-
-$local = "d:/Dev/devroot/AI-Intuition/journey11/src/test/kpubsub"
-$rmt = "/var/data"
-ProtoCompile -LocalDir $local -ConatinerDir $rmt -Filename pb_message1.proto
-ProtoCompile -LocalDir $local -ConatinerDir $rmt -Filename pb_message2.proto
-
